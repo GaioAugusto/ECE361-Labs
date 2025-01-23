4be2099d16dddc56d7d76ea85a9e3e0e3fc43652
Gaio Augusto Moraes dos Santos <gaioaugusto16@gmail.com>
2025-01-23 16:16:43 -0500

    Lab1 final version

--- lab1/deliver.c
+++ lab1/deliver.c
@@ -8,60 +8,61 @@
 #include <netinet/in.h>
 
 int main(int argc, char* argv[]){ // argc is the # of args, argv are the actual args strings
-    // 1. Check usage
+    // Check number of arguments passed in the command-line
     if (argc < 3) {
-        fprintf(stderr, "Usage: %s <server IP> <server Port>\n", argv[0]);
+        fprintf(stderr, "Command must be in the following format: %s <server IP> <server Port>\n", argv[0]);
         return 1;
     }
 
-    // 2. Parse inputs
+    // Parse inputs
     const char* serverIp = argv[1];
     int serverPort = atoi(argv[2]); // from string to int
 
-    // 3. Create udp socket
-    int socket_fd = socket(AF_INET, SOCK_DGRAM, 0); // af_init is ipv4, sock_dgram is udp, 0 is std protocol
-    if(socket_fd < 0){
+    // Create udp socket
+    int socket_fd = socket(AF_INET, SOCK_DGRAM, 0); // af_inet is ipv4, sock_dgram is udp, 0 is std protocol
+    if(socket_fd < 0){ // -1 return indicates the creation of the socket was not successful
         perror("socket");
         return 1;
     }
 
-    // 4. Set up server address structure
+    // Set up server address structure
+    // Tells the socket where to send the data
     struct sockaddr_in serverAddr;
     memset(&serverAddr, 0, sizeof(serverAddr)); // clean garbage
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(serverPort);
+    serverAddr.sin_family = AF_INET; // indicate it's ipv4
+    serverAddr.sin_port = htons(serverPort); // htons are used to convert to big-endian (used in networking protocols)
 
     // Convert the server IP (string) to a network address
-    if (inet_pton(AF_INET, serverIp, &serverAddr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, serverIp, &serverAddr.sin_addr) <= 0) { // parsing ip address to its binary form
         perror("inet_pton");
         close(socket_fd);
         return 1;
     }
 
-    // 5. Prompt for command
+    // Ask for input command
     printf("Please enter your command in the format: ftp <filename>\n");
-    char userInput[256], command[16], fileName[128];
+    char userInput[256], command[8], fileName[128];
     if(!fgets(userInput, sizeof(userInput), stdin)){
         fprintf(stderr, "Error reading input.\n");
         close(socket_fd);
         return 1;
     }
 
-    // 6. Parse user input into "command" and "fileName"
+    // Check if input is in the right format and extract command and fileName
     if (sscanf(userInput, "%s %s", command, fileName) != 2) {
         fprintf(stderr, "Invalid input. Expected: ftp <filename>\n");
         close(socket_fd);
         return 1;
     }
 
-    // 7. Verify command is "ftp"
+    // Verify command
     if (strcmp(command, "ftp") != 0) {
         fprintf(stderr, "Invalid command. Please use 'ftp <filename>'.\n");
         close(socket_fd);
         return 1;
     }
 
-    // 8. Check if file exists locally
+    // Check if file exists
     FILE *fp = fopen(fileName, "rb");
     if (!fp) {
         perror("fopen");
@@ -70,7 +71,7 @@ int main(int argc, char* argv[]){ // argc is the # of args, argv are the actual 
     }
     fclose(fp);
 
-    // 9. Send "ftp" to the server
+    // Send message to server
     const char* message = "ftp";
     ssize_t bytesSent = sendto(socket_fd, message, strlen(message), 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
     if (bytesSent < 0) {
@@ -79,9 +80,9 @@ int main(int argc, char* argv[]){ // argc is the # of args, argv are the actual 
         return 1;
     }
 
-    // 10. Receive server response
+    // Receive response
     char buffer[256];
-    memset(buffer, 0, sizeof(buffer));
+    memset(buffer, 0, sizeof(buffer)); // clear garbage data in array
     socklen_t addrLen = sizeof(serverAddr);
 
     ssize_t bytesRecv = recvfrom(socket_fd, buffer, sizeof(buffer) - 1, 0, (struct sockaddr *)&serverAddr, &addrLen);
@@ -92,15 +93,32 @@ int main(int argc, char* argv[]){ // argc is the # of args, argv are the actual 
     }
     buffer[bytesRecv] = '\0'; // Null terminate
 
-    // 11. Check server response
+    // Check server response
     if (strcmp(buffer, "yes") == 0) {
         printf("A file transfer can start.\n");
     } else {
-        printf("Server responded: %s\n", buffer);
-        printf("Exiting.\n");
+        printf("Response: %s\n", buffer);
     }
 
-    // 12. Cleanup
     close(socket_fd);
     return 0;
 }
+
+
+// Goal of the lab
+// 1. Send a message ("ftp") to a server host
+// 2. Receive a response from the server
+// 3. Determine if a file transfer may proceed based on that response
+
+// We must use UDP sockets
+
+// Parse input from the command line to get ip address and port number
+    // Ip address indicates where the server is located in the network
+    // Port number indicates which door the server is listening
+
+// Create udp socket to open the communication channel between the UG machines
+
+// Set up structure of the server address -> struct will hold details like port number and ip address
+// in a format accepted by functions like sendto() 
+
+
--- lab1/server.c
+++ lab1/server.c
@@ -1,85 +1,58 @@
-#include <stdio.h>              // For printf, perror, etc.
-#include <stdlib.h>             // For exit, atoi
-#include <string.h>             // For memset, strcmp
-#include <unistd.h>             // For close
-#include <sys/types.h>          // For system data types (optional on some systems)
-#include <sys/socket.h>         // For socket, bind, recvfrom, sendto
-#include <arpa/inet.h>          // For inet_pton, htons, etc.
-#include <netinet/in.h>         // For sockaddr_in
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
-int main(int argc, char *argv[]) {
-    // 1. Check usage: we want <server Port>
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <UDP listen port>\n", argv[0]);
-        return 1;
+int main(int argc, char* argv[]) {
+    // check arguments
+    if(argc != 2){
+        printf("[Error] Not enough or too many arguments.\n");
+        return (EXIT_FAILURE);
     }
 
-    // 2. Parse the port
-    int listenPort = atoi(argv[1]);
+    // Read the UDP port from input
+    char* port = argv[1];
 
-    // 3. Create a UDP socket
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        perror("socket");
-        return 1;
-    }
+    // Code from Beej's guide page 83 and 84
+    struct addrinfo hints;
+    struct addrinfo *res;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET; // use IPv4 or IPv6, whichever
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE; // fill in my IP for me
 
-    // 4. Set up address to bind to
-    struct sockaddr_in serverAddr;
-    memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(listenPort);
-    // Listen on any network interface (local machine). If you want a specific IP, use inet_pton, etc.
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    getaddrinfo(NULL, port, &hints, &res);
 
-    // 5. Bind the socket to our specified IP and port
-    if (bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-        perror("bind");
-        close(sockfd);
-        return 1;
-    }
+    int server_socket = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
-    // 6. Wait for a message from the client
-    printf("Server listening on port %d...\n", listenPort);
+    // bind socket to given address
+    bind(server_socket, res->ai_addr, res->ai_addrlen);
 
-    struct sockaddr_in clientAddr;
-    socklen_t clientAddrLen = sizeof(clientAddr);
-    char buffer[256];
-    memset(buffer, 0, sizeof(buffer));
+    // recv from the address
+    struct sockaddr_storage sender_address;
+    socklen_t sender_address_size = sizeof(sender_address);
+    char buffer[100];
 
-    // 7. Receive one message (blocking call)
-    ssize_t recvBytes = recvfrom(sockfd,
-                                 buffer,
-                                 sizeof(buffer)-1,  // leave space for '\0'
-                                 0,
-                                 (struct sockaddr*)&clientAddr,
-                                 &clientAddrLen);
-    if (recvBytes < 0) {
-        perror("recvfrom");
-        close(sockfd);
-        return 1;
-    }
-    // Null-terminate the message
-    buffer[recvBytes] = '\0';
+    int n = recvfrom(server_socket, buffer, 100, 0, (struct sockaddr *) &sender_address, &sender_address_size);
 
-    // 8. Check if the message is "ftp"
-    printf("Received message from client: '%s'\n", buffer);
+    // null terminate buffer
+    buffer[n] = '\0';
 
+    // if ftp send yes
     if (strcmp(buffer, "ftp") == 0) {
-        // 9. If "ftp", respond "yes"
-        const char *resp = "yes";
-        sendto(sockfd, resp, strlen(resp), 0,
-               (struct sockaddr*)&clientAddr, clientAddrLen);
-        printf("Sent response: 'yes'\n");
+        sendto(server_socket, "yes", strlen("yes"), 0, (struct sockaddr *) &sender_address, sender_address_size);
+        printf("yes\n");
     } else {
-        // Else respond "no"
-        const char *resp = "no";
-        sendto(sockfd, resp, strlen(resp), 0,
-               (struct sockaddr*)&clientAddr, clientAddrLen);
-        printf("Sent response: 'no'\n");
+        sendto(server_socket, "no", strlen("no"), 0, (struct sockaddr *) &sender_address, sender_address_size);
+        printf("no");
     }
 
-    // 10. Close the socket
-    close(sockfd);
+    close(server_socket);
+
     return 0;
 }
